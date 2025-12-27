6b7f56e8312267c8fc271289184100f27f7c710c
btheado <brian.theado@gmail.com>
2025-12-26 20:50:58 -0500

    The constructed urls use 21 bytes of the token, not 22

--- server/app.ts
+++ server/app.ts
@@ -282,7 +282,7 @@ export class TiddlyPWASyncApp {
 	@route(['GET', 'HEAD', 'OPTIONS'], '/:halftoken/:filename')
 	handleAppFile(req: Request, { halftoken, filename }: Record<string, string>) {
 		const wiki = this.db.getWikiByPrefix(halftoken);
-		if (!wiki || halftoken.length < 22) {
+		if (!wiki || halftoken.length < 21) {
 			return Response.json({ error: 'EEXIST' }, { headers: respHdrs, status: 404 });
 		}
 		if (req.method === 'OPTIONS') {
